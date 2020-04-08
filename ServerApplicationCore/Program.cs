20e3a26eca427302b448fa29fbe7e08b0a53f787
Viktor Morin <viktor.morin@vattenfall.com>
2020-04-08 15:51:31 +0200

    added connected clients as output

--- ServerApplicationCore/Program.cs
+++ ServerApplicationCore/Program.cs
@@ -4,6 +4,8 @@ namespace ServerApplicationCore
 {
     class Program
     {
+        private EasyModbusCore.ModbusServer modbusServer;
+
         static void Main(string[] args)
         {
             Program application = new Program();
@@ -12,8 +14,9 @@ namespace ServerApplicationCore
 
         public void startServer()
         {
-            EasyModbusCore.ModbusServer modbusServer = new EasyModbusCore.ModbusServer();
+            modbusServer = new EasyModbusCore.ModbusServer();
             modbusServer.Listen();
+            modbusServer.NumberOfConnectedClientsChanged += ModbusServer_NumberOfConnectedClientsChanged;
             modbusServer.HoldingRegistersChanged += new EasyModbusCore.ModbusServer.HoldingRegistersChangedHandler(holdingRegistersChanged);
             Console.WriteLine("[Modbus Server][INFO] - press any key to exit application");
             Console.WriteLine("[Modbus Server][INFO] - started");
@@ -21,6 +24,11 @@ namespace ServerApplicationCore
             Console.ReadKey();
             Console.WriteLine("[Modbus Server][INFO] - terminated");
             modbusServer.StopListening();
+        }
+
+        private void ModbusServer_NumberOfConnectedClientsChanged()
+        {
+            Console.WriteLine($"Connected clients: \t {modbusServer.NumberOfConnections}");
         }
 
         public void holdingRegistersChanged(int startingAddress, int quantity)
